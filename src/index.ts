da9390ecfbf1e292f6105ad4a168bc52803ab223
Tim Bedsole <tim@Tims-MacBook-Air.local>
2024-05-06 22:45:00 -0400

    Added User class, get and set methods

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log(`Hi there`);
+import { User } from './models/User';
+
+const user = new User({ name: 'myName', age: 20 });
+
+user.set({ name: 'newName' });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
