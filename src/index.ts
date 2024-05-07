30801364dae830319175680a7e801ec57545717e
Tim Bedsole <tim@Tims-MacBook-Air.local>
2024-05-07 19:40:19 -0400

    Added framework for custom event handling methon on User class

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { User } from './models/User';
 
 const user = new User({ name: 'myName', age: 20 });
 
-user.set({ name: 'newName' });
+user.on('change', () => {});
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+console.log(user);
